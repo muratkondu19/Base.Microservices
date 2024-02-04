94be1dc8b9461b5b70daa8a6ca519efac4b6e22f
Murat Kondu <murat.kondu.mk@hotmail.com>
2024-02-04 22:13:54 +0300

    Services.Catolog CourseController eklendi Shared.CustomBaseController eklendi

---
+++ Services/Catolog/FreeCourse.Services.Catolog/Controllers/CoursesController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FreeCourse.Services.Catolog.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CoursesController : ControllerBase {
+    }
+}
---
+++ Shared/FreeCourse.Shared/ControllerBases/CustomBaseController.cs
@@ -0,0 +1,22 @@
+﻿using FreeCourse.Shared.Dtos;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Shared.ControllerBases {
+    public class CustomBaseController : ControllerBase {
+        //ControllerBase erişimi için Project/right click/edit project file
+        //  <ItemGroup>
+        //  <FrameworkReference Include = "Microsoft.AspNetCore.App" />
+        //</ ItemGroup >
+        //file içerisine bu prop eklenerek controller base sınıfına erişim sağlanabilir.
+        public IActionResult CreateActionResultInstance<T>(Response<T> response) {
+            return new ObjectResult(response) {
+                StatusCode = response.StatusCode
+            };
+        }
+    }
+}
