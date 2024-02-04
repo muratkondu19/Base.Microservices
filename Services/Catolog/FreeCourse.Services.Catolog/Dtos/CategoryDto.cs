f51eeca083dcd23449a94aeca227abec07243f48
Murat Kondu <murat.kondu.mk@hotmail.com>
2024-02-04 18:53:50 +0300

    Catolog.API Dto nesnelerinin oluşturulması

---
+++ Services/Catolog/FreeCourse.Services.Catolog/Dtos/CategoryDto.cs
@@ -0,0 +1,7 @@
+﻿namespace FreeCourse.Services.Catolog.Dtos {
+    public class CategoryDto {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
---
+++ Services/Catolog/FreeCourse.Services.Catolog/Dtos/FeatureDto.cs
@@ -0,0 +1,5 @@
+﻿namespace FreeCourse.Services.Catolog.Dtos {
+    public class FeatureDto {
+        public int Duration { get; set; }
+    }
+}
