cb2e5dce5bb600ceffbc1409cd559faa441f29c5
Murat Kondu <murat.kondu.mk@hotmail.com>
2024-02-10 23:30:43 +0300

    BasketService appsettings üzerinde redis conf tanımlama

--- Services/Basket/FreeCourse.Basket/Program.cs
+++ Services/Basket/FreeCourse.Basket/Program.cs
@@ -1,3 +1,7 @@
+using FreeCourse.Services.Basket.Settings;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.Configure<RedisSettings>(builder.Configuration.GetSection("RedisSettings"));
+
 
 var app = builder.Build();
 
