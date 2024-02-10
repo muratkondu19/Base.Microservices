004448558ae2e40c04237c7d254e8b3a105e666e
Murat Kondu <murat.kondu.mk@hotmail.com>
2024-02-10 09:36:38 +0300

    IdentityServer - Signup endpoint hazırlandı Shared projesi .net 3.1 configure edildi (identityserver desteğinden ötürü)

---
+++ IdentityServer/Base.Microservices.IdentityServer/Controllers/UserController.cs
@@ -0,0 +1,42 @@
+﻿using Base.Microservices.IdentityServer.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using static IdentityServer4.IdentityServerConstants;
+using System.Linq;
+using System.Threading.Tasks;
+using Base.Microservices.IdentityServer.Dtos;
+using FreeCourse.Shared.Dtos;
+
+namespace Base.Microservices.IdentityServer.Controllers {
+    [Authorize(LocalApi.PolicyName)]
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class UserController : ControllerBase {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UserController(UserManager<ApplicationUser> userManager) {
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignUp(SignupDto signupDto) {
+            var user = new ApplicationUser {
+                UserName = signupDto.UserName,
+                Email = signupDto.Email,
+                City = signupDto.City
+            };
+
+            //Password hashleneceği için burada belirtilir. 
+            var result = await _userManager.CreateAsync(user, signupDto.Password);
+
+            if (!result.Succeeded) {
+                return BadRequest(Response<NoContent>.Fail(result.Errors.Select(x => x.Description).ToList(), 400));
+            }
+
+            return NoContent();
+        }
+
+    }
+}
