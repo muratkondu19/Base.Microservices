33a0c91c46441c4cf8a9baa7f62efaec1a977716
Murat Kondu <murat.kondu.mk@hotmail.com>
2024-02-10 22:48:21 +0300

    photostock api mikroservisi koruma altına alınması

--- Services/PhotoStock/FreeCourse.PhotoStock/Program.cs
+++ Services/PhotoStock/FreeCourse.PhotoStock/Program.cs
@@ -1,10 +1,23 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Mvc.Authorization;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(opt => {
+    //tüm controllerý auhroize attr eklemek için kullanýlýr
+    opt.Filters.Add(new AuthorizeFilter());
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+
+//mikroservisi koruma altýna alma
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options => {
+    options.Authority = builder.Configuration["IdentityServerURL"]; //bu mikroservise tokený kim daðýtýtyor 
+    options.Audience = "resource_catalog"; //birden fazla belirtilemiyor 
+    options.RequireHttpsMetadata = false; //default https beklediði için false set ediyoruz
+});
 
 
 var app = builder.Build();
@@ -19,7 +32,7 @@ app.UseStaticFiles();
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
-
+app.UseAuthentication();
 app.MapControllers();
 
 app.Run();
