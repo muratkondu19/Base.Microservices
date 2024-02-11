a3d6aea678e84acb3433377cad92e447c25715b0
Murat Kondu <murat.kondu.mk@hotmail.com>
2024-02-11 12:54:24 +0300

    Discount model oluşturulması ve dapper kütüphanesi kurulumu

---
+++ Services/Discount/FreeCourse.Services.Discount/Models/Discount.cs
@@ -0,0 +1,12 @@
+﻿namespace FreeCourse.Services.Discount.Models {
+
+    //postgre tarafında table ile modeli maplemek için kullanılır
+    [Dapper.Contrib.Extensions.Table("discount")]
+    public class Discount {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public int Rate { get; set; }
+        public string Code { get; set; }
+        public DateTime CreatedTime { get; set; }
+    }
+}
