1a5b8851b0c472ead789753d103f7336ec2668cc
Murat Kondu <murat.kondu.mk@hotmail.com>
2024-02-11 11:55:43 +0300

    Basket mikroservisinin koruma altına alınması ve auth user zorunluluğu policy tanımanması

--- Services/Basket/FreeCourse.Basket/Program.cs
+++ Services/Basket/FreeCourse.Basket/Program.cs
@@ -1,14 +1,24 @@
 using FreeCourse.Services.Basket.Services;
 using FreeCourse.Services.Basket.Settings;
 using FreeCourse.Shared.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Options;
+using System.Net;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var requireAuthorizePolicy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
 
-builder.Services.AddControllers();
+// Add services to the container.
+builder.Services.AddControllers(opt => {
+    //tüm controllerý auhroize attr eklemek için kullanýlýr
+    //mutlaka authenticate olmuþ bir user gerekmektedir.
+    opt.Filters.Add(new AuthorizeFilter(requireAuthorizePolicy));
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -29,6 +39,12 @@ builder.Services.AddSingleton<RedisService>(sp =>
 //IShared identity servis eklenerek user id deðerini metod üzerinden okuma saðlanacak
 builder.Services.AddScoped<ISharedIdentityService,SharedIdentityService>();
 
+//mikroservisi koruma altýna alma
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options => {
+    options.Authority = builder.Configuration["IdentityServerURL"]; //bu mikroservise tokený kim daðýtýtyor 
+    options.Audience = "resource_catalog"; //birden fazla belirtilemiyor 
+    options.RequireHttpsMetadata = false; //default https beklediði için false set ediyoruz
+});
 
 var app = builder.Build();
 
@@ -41,7 +57,7 @@ if (app.Environment.IsDevelopment()) {
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
-
+app.UseAuthentication();
 app.MapControllers();
 
 app.Run();
