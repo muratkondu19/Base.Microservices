814ae3f4d9297ad7125903e11809e7201146c668
Murat Kondu <murat.kondu.mk@hotmail.com>
2024-02-15 16:47:28 +0300

    IIdentityService/IdentityModel Library implementasyon

---
+++ Frontend/FreeCourse.Web/Models/SigninInput.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace FreeCourse.Web.Models {
+    //kullanıcıdan veri alınacaksa class son eki input
+    //kullanıcıya veri gösterilecekse class son eki viewmodel olacak
+    public class SigninInput {
+        [Required]
+        [Display(Name = "Email adresiniz")]
+        public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "Şifreniz")]
+        public string Password { get; set; }
+
+        [Display(Name = "Beni hatırla")]
+        public bool IsRemember { get; set; }
+    }
+}
---
+++ Frontend/FreeCourse.Web/Services/Interface/IIdentityService.cs
@@ -0,0 +1,15 @@
+﻿using FreeCourse.Shared.Dtos;
+using FreeCourse.Web.Models;
+using IdentityModel.Client;
+
+namespace FreeCourse.Web.Services.Interface {
+    public interface IIdentityService {
+        Task<Response<bool>> SignIn(SigninInput signinInput);
+
+        //token response identitymodel sınıfından gelmektedir ve token verilerini taşımaktadır
+        Task<TokenResponse> GetAccessTokenByRefreshToken();
+
+        //logout işlemlerinde kullanılacak
+        Task RevokeRefreshToken();
+    }
+}
