f3e3fd0c278daceeb7f0a8b36fe1305d39fd7257
Murat Kondu <murat.kondu.mk@hotmail.com>
2024-02-17 23:55:02 +0300

    IPaymentService/PaymentService

---
+++ Frontend/FreeCourse.Web/Services/Interface/IPaymentService.cs
@@ -0,0 +1,7 @@
+﻿using FreeCourse.Web.Models.FakePayments;
+
+namespace FreeCourse.Web.Services.Interface {
+    public interface IPaymentService {
+        Task<bool> ReceivePayment(PaymentInfoInput paymentInfoInput);
+    }
+}
---
+++ Frontend/FreeCourse.Web/Services/PaymentService.cs
@@ -0,0 +1,10 @@
+﻿using FreeCourse.Web.Models.FakePayments;
+using FreeCourse.Web.Services.Interface;
+
+namespace FreeCourse.Web.Services {
+    public class PaymentService : IPaymentService {
+        public Task<bool> ReceivePayment(PaymentInfoInput paymentInfoInput) {
+            throw new NotImplementedException();
+        }
+    }
+}
