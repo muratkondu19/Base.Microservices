65da41283957ba2a5296ad4bd0c11b30c94d8535
Murat Kondu <murat.kondu.mk@hotmail.com>
2024-02-17 23:49:20 +0300

    Basket ve discount service test ve bug fix

--- Frontend/FreeCourse.Web/Validators/DiscountApplyInputValidator.cs
+++ Frontend/FreeCourse.Web/Validators/DiscountApplyInputValidator.cs
@@ -1,4 +1,7 @@
-﻿namespace FreeCourse.Web.Validators {
+﻿using FluentValidation;
+using FreeCourse.Web.Models.Discounts;
+
+namespace FreeCourse.Web.Validators {
     public class DiscountApplyInputValidator : AbstractValidator<DiscountApplyInput> {
         public DiscountApplyInputValidator() {
             RuleFor(x => x.Code).NotEmpty().WithMessage("indirim kupon alanı boş olamaz");
