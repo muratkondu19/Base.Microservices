b9dceecd9f7add97b5dbbb83ae30a07c73853340
Murat Kondu <murat.kondu.mk@hotmail.com>
2024-02-17 23:13:01 +0300

    BasketController ApplyDiscount Action Method/CancelApplyDiscount Action Method

--- Frontend/FreeCourse.Web/Controllers/BasketController.cs
+++ Frontend/FreeCourse.Web/Controllers/BasketController.cs
@@ -1,4 +1,5 @@
 ﻿using FreeCourse.Web.Models.Baskets;
+using FreeCourse.Web.Models.Discounts;
 using FreeCourse.Web.Services.Interface;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -31,6 +32,21 @@ namespace FreeCourse.Web.Controllers {
         public async Task<IActionResult> RemoveBasketItem(string courseId) {
             var result = await _basketService.RemoveBasketItem(courseId);
 
+            return RedirectToAction(nameof(Index));
+        }
+        public async Task<IActionResult> ApplyDiscount(DiscountApplyInput discountApplyInput) {
+            if (!ModelState.IsValid) {
+                TempData["discountError"] = ModelState.Values.SelectMany(x => x.Errors).Select(x => x.ErrorMessage).First();
+                return RedirectToAction(nameof(Index));
+            }
+            var discountStatus = await _basketService.ApplyDiscount(discountApplyInput.Code);
+
+            TempData["discountStatus"] = discountStatus;
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> CancelApplyDiscount() {
+            await _basketService.CancelApplyDiscount();
             return RedirectToAction(nameof(Index));
         }
 
