e13d1e084612be13a6224d62ddc3462c133b4835
Murat Kondu <murat.kondu.mk@hotmail.com>
2024-02-17 19:51:17 +0300

    BasketService Methods

--- Frontend/FreeCourse.Web/Services/BasketService.cs
+++ Frontend/FreeCourse.Web/Services/BasketService.cs
@@ -1,34 +1,103 @@
-﻿using FreeCourse.Web.Models.Baskets;
+﻿using FreeCourse.Shared.Dtos;
+using FreeCourse.Web.Models.Baskets;
 using FreeCourse.Web.Services.Interface;
 
 namespace FreeCourse.Web.Services {
     public class BasketService : IBasketService {
-        public Task AddBasketItem(BasketItemViewModel basketItemViewModel) {
-            throw new NotImplementedException();
+        private readonly HttpClient _httpClient;
+
+        public BasketService(HttpClient httpClient) {
+            _httpClient = httpClient;
         }
 
-        public Task<bool> ApplyDiscount(string discountCode) {
-            throw new NotImplementedException();
+        public async Task AddBasketItem(BasketItemViewModel basketItemViewModel) {
+            var basket = await Get();
+
+            if (basket != null) {
+                if (!basket.BasketItems.Any(x => x.CourseId == basketItemViewModel.CourseId)) {
+                    basket.BasketItems.Add(basketItemViewModel);
+                }
+            }
+            else {
+                basket = new BasketViewModel();
+
+                basket.BasketItems.Add(basketItemViewModel);
+            }
+
+            await SaveOrUpdate(basket);
         }
 
-        public Task<bool> CancelApplyDiscount() {
-            throw new NotImplementedException();
+        public async Task<bool> ApplyDiscount(string discountCode) {
+            await CancelApplyDiscount();
+
+            var basket = await Get();
+            if (basket == null) {
+                return false;
+            }
+
+            await SaveOrUpdate(basket);
+            return true;
         }
 
-        public Task<bool> Delete() {
-            throw new NotImplementedException();
+        public async Task<bool> CancelApplyDiscount() {
+            var basket = await Get();
+
+            if (basket == null || basket.DiscountCode == null) {
+                return false;
+            }
+
+            basket.CancelDiscount();
+            await SaveOrUpdate(basket);
+            return true;
         }
 
-        public Task<BasketViewModel> Get() {
-            throw new NotImplementedException();
+        public async Task<bool> Delete() {
+            var result = await _httpClient.DeleteAsync("baskets");
+
+            return result.IsSuccessStatusCode;
         }
 
-        public Task<bool> RemoveBasketItem(string courseId) {
-            throw new NotImplementedException();
+        public async Task<BasketViewModel> Get() {
+            var response = await _httpClient.GetAsync("baskets");
+
+            if (!response.IsSuccessStatusCode) {
+                return null;
+            }
+            var basketViewModel = await response.Content.ReadFromJsonAsync<Response<BasketViewModel>>();
+
+            return basketViewModel.Data;
         }
 
-        public Task<bool> SaveOrUpdate(BasketViewModel basketViewModel) {
-            throw new NotImplementedException();
+        public async Task<bool> RemoveBasketItem(string courseId) {
+            var basket = await Get();
+
+            if (basket == null) {
+                return false;
+            }
+
+            var deleteBasketItem = basket.BasketItems.FirstOrDefault(x => x.CourseId == courseId);
+
+            if (deleteBasketItem == null) {
+                return false;
+            }
+
+            var deleteResult = basket.BasketItems.Remove(deleteBasketItem);
+
+            if (!deleteResult) {
+                return false;
+            }
+
+            if (!basket.BasketItems.Any()) {
+                basket.DiscountCode = null;
+            }
+
+            return await SaveOrUpdate(basket);
+        }
+
+        public async Task<bool> SaveOrUpdate(BasketViewModel basketViewModel) {
+            var response = await _httpClient.PostAsJsonAsync<BasketViewModel>("baskets", basketViewModel);
+
+            return response.IsSuccessStatusCode;
         }
     }
 }
